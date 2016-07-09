36c51818d3a09d281278faf6aa412ef1681bb9f9
Jeevanandam M <jeeva@myjeeva.com>
2016-07-09 00:20:48 -0700

    version number added

--- essentials.go
+++ essentials.go
@@ -1,7 +1,10 @@
 // Copyright (c) 2016 Jeevanandam M (https://github.com/jeevatkm)
-// resty source code and usage is governed by a MIT style
+// go-aah/essentails source code and usage is governed by a MIT style
 // license that can be found in the LICENSE file.
 
 // Package ess provides simple & useful utils for Go. aah framework utilizes
 // essentails library across. It's pretty handy you can use it too :)
 package ess
+
+// Version no. of go-aah/essentails library
+var Version = "0.1"
