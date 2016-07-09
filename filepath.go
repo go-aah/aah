5fef8ff71c827d97771a181b6a439fa7fd160da6
Jeevanandam M <jeeva@myjeeva.com>
2016-07-09 00:12:27 -0700

    file interaction methods

---
+++ filepath.go
@@ -0,0 +1,226 @@
+// Copyright (c) 2016 Jeevanandam M (https://github.com/jeevatkm)
+// resty source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package ess
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Excludes is handly filepath match manipulation
+type Excludes []string
+
+// Validate helps to evalute the pattern are valid
+// `Match` method is from error and focus on match
+func (e *Excludes) Validate() error {
+	for _, pattern := range *e {
+		if _, err := filepath.Match(pattern, "abc/b/c"); err != nil {
+			return fmt.Errorf("unable to evalute pattern: %v", pattern)
+		}
+	}
+	return nil
+}
+
+// Match evalutes given file with available patterns returns true if matches
+// otherwise false. `Match` internally uses the `filepath.Match`
+//
+// Note: `Match` ignore pattern errors, use `Validate` method to ensure
+// you have correct exclude patterns
+func (e *Excludes) Match(file string) bool {
+	for _, pattern := range *e {
+		if match, _ := filepath.Match(pattern, file); match {
+			return match
+		}
+	}
+	return false
+}
+
+// IsFileExists return true is file or directory is exists, otherwise returns
+// false. It also take cares of symlink path as well
+func IsFileExists(filename string) bool {
+	_, err := os.Lstat(filename)
+	return err == nil
+}
+
+// IsDirEmpty returns true if the given directory is empty also returns true if
+// directory not exists. Otherwise returns false
+func IsDirEmpty(path string) bool {
+	if !IsFileExists(path) {
+		// directory not exists
+		return true
+	}
+	dir, _ := os.Open(path)
+	defer CloseQuietly(dir)
+	results, _ := dir.Readdir(1)
+	return len(results) == 0
+}
+
+// ApplyFileMode applies the given file mode to the target{file|directory}
+func ApplyFileMode(target string, mode os.FileMode) error {
+	err := os.Chmod(target, mode)
+	if err != nil {
+		return fmt.Errorf("unable to apply mode: %v, to given file or directory: %v", mode, target)
+	}
+	return nil
+}
+
+// LineCnt counts no. of lines on file
+func LineCnt(fileName string) int {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return 0
+	}
+	defer CloseQuietly(f)
+
+	return LineCntr(f)
+}
+
+// LineCntr counts no. of lines for given reader
+func LineCntr(r io.Reader) int {
+	buf := make([]byte, 8196)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := r.Read(buf)
+		if err != nil && err != io.EOF {
+			return count
+		}
+
+		count += bytes.Count(buf[:c], lineSep)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return count
+}
+
+// Walk method extends filepath.Walk to also follows symlinks.
+// Always returns the path of the file or directory also path
+// is inline to name of symlink
+func Walk(srcDir string, walkFn filepath.WalkFunc) error {
+	return doWalk(srcDir, srcDir, walkFn)
+}
+
+func doWalk(fname string, linkName string, walkFn filepath.WalkFunc) error {
+	fsWalkFn := func(path string, info os.FileInfo, err error) error {
+		var name string
+		name, err = filepath.Rel(fname, path)
+		if err != nil {
+			return err
+		}
+
+		path = filepath.Join(linkName, name)
+
+		if err == nil && info.Mode()&os.ModeSymlink == os.ModeSymlink {
+			var symlinkPath string
+			symlinkPath, err = filepath.EvalSymlinks(path)
+			if err != nil {
+				return err
+			}
+
+			// https://github.com/golang/go/blob/master/src/path/filepath/path.go#L392
+			info, err = os.Lstat(symlinkPath)
+			if err != nil {
+				return walkFn(path, info, err)
+			}
+
+			if info.IsDir() {
+				return doWalk(symlinkPath, path, walkFn)
+			}
+		}
+
+		return walkFn(path, info, err)
+	}
+
+	return filepath.Walk(fname, fsWalkFn)
+}
+
+// CopyFile copies the given source file into destination
+func CopyFile(dest, src string) (int64, error) {
+	if !IsFileExists(src) {
+		return 0, fmt.Errorf("source file is not exists: %v", src)
+	}
+
+	if IsFileExists(dest) {
+		return 0, fmt.Errorf("destination file already exists: %v", dest)
+	}
+
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return 0, fmt.Errorf("unable to create dest file: %v", dest)
+	}
+	defer CloseQuietly(destFile)
+
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return 0, fmt.Errorf("unable to open source file: %v", src)
+	}
+	defer CloseQuietly(srcFile)
+
+	copiedBytes, err := io.Copy(destFile, srcFile)
+	if err != nil {
+		return 0, fmt.Errorf("unable to copy file from %v to %v (%v)",
+			src, dest, err)
+	}
+
+	return copiedBytes, nil
+}
+
+// CopyDir copies entire directory, sub directories and files into destination
+// and it excludes give file matches
+func CopyDir(dest, src string, excludes Excludes) error {
+	if !IsFileExists(src) {
+		return fmt.Errorf("source dir is not exists: %v", src)
+	}
+
+	src = filepath.Clean(src)
+	srcInfo, _ := os.Lstat(src)
+	if !srcInfo.IsDir() {
+		return fmt.Errorf("source is not directory: %v", src)
+	}
+
+	if IsFileExists(dest) {
+		return fmt.Errorf("destination dir already exists: %v", dest)
+	}
+
+	if err := excludes.Validate(); err != nil {
+		return err
+	}
+
+	return Walk(src, func(srcPath string, info os.FileInfo, err error) error {
+		if excludes.Match(filepath.Base(srcPath)) {
+			if info.IsDir() {
+				// excluding directory
+				return filepath.SkipDir
+			}
+			// excluding file
+			return nil
+		}
+
+		relativeSrcPath := strings.TrimLeft(srcPath[len(src):], string(filepath.Separator))
+		destPath := filepath.Join(dest, relativeSrcPath)
+
+		if info.IsDir() {
+			// directory permisions is not preserved from source
+			return MkDirAll(destPath, 0755)
+		}
+
+		// copy source into destination
+		if _, err = CopyFile(destPath, srcPath); err != nil {
+			return err
+		}
+
+		// Apply source permision into target as well
+		// so file permissions are preserved
+		return ApplyFileMode(destPath, info.Mode())
+	})
+}
---
+++ filepath_test.go
@@ -0,0 +1,171 @@
+// Copyright (c) 2016 Jeevanandam M (https://github.com/jeevatkm)
+// resty source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package ess
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestIsFileExists(t *testing.T) {
+	assertEqual(t, "TestIsFileExists", true, IsFileExists("testdata/sample.txt"))
+	assertEqual(t, "TestIsFileExists", false, IsFileExists("testdata/sample-not-exists.txt"))
+
+	assertEqual(t, "TestIsFileExists", true, IsFileExists("testdata"))
+	assertEqual(t, "TestIsFileExists", false, IsFileExists("testdata-not-exists"))
+}
+
+func TestIsDirEmpty(t *testing.T) {
+	assertEqual(t, "TestIsDirEmpty", false, IsDirEmpty("testdata"))
+	assertEqual(t, "TestIsDirEmpty", true, IsDirEmpty("testdata-not-exists.txt"))
+
+	_ = MkDirAll("testdata/path/isdirempty", 0755)
+	assertEqual(t, "TestIsDirEmpty", true, IsDirEmpty("testdata/path/isdirempty"))
+}
+
+func TestApplyFileMode(t *testing.T) {
+	fileName := "testdata/FileMode.txt"
+	err := ioutil.WriteFile(fileName,
+		[]byte(`This file is for file permission testing`), 0700)
+	failOnError(t, err)
+
+	fileInfo, err := os.Stat(fileName)
+	failOnError(t, err)
+	if fileInfo.Mode() != os.FileMode(0700) {
+		t.Errorf("expected file mode: 0700 got %v", fileInfo.Mode())
+	}
+
+	err = ApplyFileMode(fileName, 0755)
+	failOnError(t, err)
+
+	fileInfo, err = os.Stat(fileName)
+	failOnError(t, err)
+	if fileInfo.Mode() != os.FileMode(0755) {
+		t.Errorf("expected file mode: 0755 got %v", fileInfo.Mode())
+	}
+
+	// expected to fail
+	err = ApplyFileMode("/var", 0755)
+	if err == nil {
+		t.Error("Expected error got nil")
+	}
+
+	removeFiles(fileName)
+}
+
+func TestLineCntByFilePath(t *testing.T) {
+	count := LineCnt("testdata/sample.txt")
+	assertEqual(t, "TestLineCntByFilePath", 20, count)
+
+	count = LineCnt("testdata/sample-not.txt")
+	assertEqual(t, "TestLineCntByFilePath", 0, count)
+}
+
+func TestLineCntByReader(t *testing.T) {
+	file, err := os.Open("testdata/sample.txt")
+	failOnError(t, err)
+	defer CloseQuietly(file)
+
+	assertEqual(t, "TestLineCntByReader", 20, LineCntr(file))
+}
+
+func TestWalk(t *testing.T) {
+	pwd, _ := os.Getwd()
+	fileName := filepath.Join(pwd, "testdata/symlinktest.txt")
+	err := ioutil.WriteFile(fileName,
+		[]byte(`This file is for file permission testing 1`), 0755)
+	failOnError(t, err)
+
+	err = MkDirAll("testdata/symlinkdata", 0755)
+	failOnError(t, err)
+
+	err = ioutil.WriteFile("testdata/symlinkdata/file1.txt",
+		[]byte(`This file is for file permission testing 2`), 0755)
+	failOnError(t, err)
+
+	// preparing symlink for test
+	newName1 := filepath.Join(pwd, "testdata/symlinktest1.txt")
+	err = os.Symlink(fileName, newName1)
+	failOnError(t, err)
+
+	newName2 := filepath.Join(pwd, "testdata/symlinktest2.txt")
+	err = os.Symlink(fileName, newName2)
+	failOnError(t, err)
+
+	newName3 := filepath.Join(pwd, "testdata/symlinkdata1")
+	err = os.Symlink(filepath.Join(pwd, "testdata/symlinkdata"), newName3)
+	failOnError(t, err)
+
+	err = CopyDir("/tmp/symlinktest", "testdata", Excludes{})
+	failOnError(t, err)
+
+	removeFiles(fileName, newName1, newName2)
+	removeAllFiles("testdata/symlinkdata", "/tmp/symlinktest", "testdata/symlinkdata1")
+}
+
+func TestExcludes(t *testing.T) {
+	errExcludes := Excludes{
+		".*",
+		"DS_Store.bak",
+		"[^",
+		"[]a]",
+	}
+	assertEqual(t, "TestExcludes fail", true, errExcludes.Validate() != nil)
+
+	excludes := Excludes{
+		".*",
+		"*.bak",
+		"*.tmp",
+		"tmp",
+	}
+	assertEqual(t, "TestExcludes success", true, excludes.Validate() == nil)
+}
+
+func TestCopyFile(t *testing.T) {
+	_, err := CopyFile("testdata/file-found.txt", "testdata/file-not-exists.txt")
+	if err == nil {
+		t.Error("Expected error got nil")
+	}
+
+	_, err = CopyFile("testdata/sample.txt", "testdata/sample.txt")
+	if err == nil {
+		t.Error("Expected error got nil")
+	}
+
+	_, err = CopyFile("/var/you-will-not-be-able-to-create.txt", "testdata/sample.txt")
+	if err == nil {
+		t.Error("Expected error got nil")
+	}
+}
+
+func TestCopyDir(t *testing.T) {
+	err := CopyDir("/tmp/target", "testdata/not-exists-dir", Excludes{})
+	if err == nil {
+		t.Error("Expected error got nil")
+	}
+
+	err = CopyDir("/tmp/target", "testdata/sample.txt", Excludes{})
+	if err == nil {
+		t.Error("Expected error got nil")
+	}
+
+	err = CopyDir("/tmp", "testdata", Excludes{})
+	if err == nil {
+		t.Error("Expected error got nil")
+	}
+
+	err = CopyDir("/tmp/target", "testdata", Excludes{"[]a]"})
+	if err == nil {
+		t.Error("Expected error got nil")
+	}
+
+	pwd, _ := os.Getwd()
+	err = CopyDir("/tmp/test1", pwd, Excludes{"test*", "*conf", ".*"})
+	failOnError(t, err)
+
+	removeAllFiles("/tmp/test1")
+}
