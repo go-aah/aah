0292199c86c787df91b4d06edd8150ef02f65e9a
Jeevanandam M <jeeva@myjeeva.com>
2016-07-11 11:33:07 -0700

    adding aah starting...

---
+++ aah.go
@@ -0,0 +1,5 @@
+// Copyright (c) 2016 Jeevanandam M (https://github.com/jeevatkm)
+// go-aah/aah source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package aah
