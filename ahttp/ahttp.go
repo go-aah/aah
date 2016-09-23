e241de66a162ea9e4c9f590390f0d7b557b4fe41
Jeevanandam M <jeeva@myjeeva.com>
2016-09-23 16:24:39 -0700

    accept, accept-language, content-type header manipulation

---
+++ ahttp/ahttp.go
@@ -0,0 +1,24 @@
+// Copyright (c) Jeevanandam M (https://github.com/jeevatkm)
+// go-aah/aah source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+// Package ahttp is to cater HTTP helper methods for aah framework.
+// Like parse HTTP headers, ResponseWriter, content type, etc.
+package ahttp
+
+type (
+	// Locale value is negotiated from HTTP header `Accept-Language`
+	Locale struct {
+		Raw      string
+		Language string
+		Region   string
+	}
+
+	// ContentType is represents request and response content type values
+	ContentType struct {
+		Raw    string
+		Mime   string
+		Exts   []string
+		Params map[string]string
+	}
+)
