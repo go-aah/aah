1d444d90bac7f4a6c92c80e80ec6e9104566ada0
Jeevanandam M <jeeva@myjeeva.com>
2016-09-24 16:21:35 -0700

    test coverage and cleanup

---
+++ router_test.go
@@ -0,0 +1,24 @@
+// Copyright (c) Jeevanandam M (https://github.com/jeevatkm)
+// go-aah/aah source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package router
+
+import (
+	"testing"
+
+	"github.com/go-aah/test/assert"
+)
+
+func TestPathParamGet(t *testing.T) {
+	pathParameters := pathParams{
+		pathParam{"dir", "js"},
+		pathParam{"filepath", "/inc/framework.js"},
+	}
+
+	fp := pathParameters.Get("filepath")
+	assert.Equal(t, "/inc/framework.js", fp)
+
+	notfound := pathParameters.Get("notfound")
+	assert.Equal(t, "", notfound)
+}
