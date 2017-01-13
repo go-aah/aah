a34576b37b3062761171a94cd26ecf5778431112
Jeevanandam M <jeeva@myjeeva.com>
2017-01-12 23:57:10 -0800

    ast info types added

--- controller.go
+++ controller.go
@@ -4,12 +4,80 @@
 
 package aah
 
-import "aahframework.org/aah/ahttp"
+import (
+	"fmt"
+	"path/filepath"
 
-// Controller type for aah framework, gets embedded in application controller.
-type Controller struct {
-	// Req is HTTP request instance
-	Req *ahttp.Request
+	"aahframework.org/aah/ahttp"
+	"aahframework.org/essentials"
+)
 
-	res ahttp.ResponseWriter
+type (
+	// Controller type for aah framework, gets embedded in application controller.
+	Controller struct {
+		// Req is HTTP request instance
+		Req *ahttp.Request
+
+		res ahttp.ResponseWriter
+	}
+
+	// TypeInfo holds the information about Controller Name, Methods,
+	// Embedded types etc.
+	TypeInfo struct {
+		Name          string
+		ImportPath    string
+		Methods       []*MethodInfo
+		EmbeddedTypes []*TypeInfo
+	}
+
+	// MethodInfo holds the information of single method and it's Parameters.
+	MethodInfo struct {
+		Name       string
+		StructName string
+		Parameters []*ParameterInfo
+	}
+
+	// ParameterInfo holds the information of single Parameter in the method.
+	ParameterInfo struct {
+		Name       string
+		ImportPath string
+		Type       *TypeExpr
+	}
+
+	// TypeExpr holds the information of single parameter data type.
+	TypeExpr struct {
+		Expr         string
+		IsBuiltIn    bool
+		PackageName  string
+		ImportPath   string
+		PackageIndex uint8
+		Valid        bool
+	}
+)
+
+//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+// TypeInfo methods
+//___________________________________
+
+// FullyQualifiedName method returns the fully qualified type name.
+func (t *TypeInfo) FullyQualifiedName() string {
+	return fmt.Sprintf("%s.%s", t.ImportPath, t.Name)
+}
+
+// PackageName method returns types package name from import path.
+func (t *TypeInfo) PackageName() string {
+	return filepath.Base(t.ImportPath)
+}
+
+//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+// TypeExpr methods
+//___________________________________
+
+// Name method returns type name for expression.
+func (te *TypeExpr) Name() string {
+	if te.IsBuiltIn || ess.IsStrEmpty(te.PackageName) {
+		return te.Expr
+	}
+
+	return fmt.Sprintf("%s%s.%s", te.Expr[:te.PackageIndex], te.PackageName, te.Expr[te.PackageIndex:])
 }
