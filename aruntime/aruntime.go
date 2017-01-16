20b481ebf6c3da69ecab9322920fc17a9ff1dda5
Jeevanandam M <jeeva@myjeeva.com>
2017-01-15 16:31:05 -0800

    go stacktrace parsing is done

---
+++ aruntime/aruntime.go
@@ -0,0 +1,5 @@
+// Copyright (c) Jeevanandam M (https://github.com/jeevatkm)
+// go-aah/aah source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package aruntime
