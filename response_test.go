f481e50195cf9faa33ad31cf184acf5a41ff2a9c
Jeevanandam M <jeeva@myjeeva.com>
2017-01-16 17:21:06 -0800

    write header now and test added

--- response_test.go
+++ response_test.go
@@ -10,7 +10,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
+	"time"
 
 	"aahframework.org/test/assert"
 )
@@ -33,6 +35,73 @@ func TestResponseWriter(t *testing.T) {
 		_ = writer.Unwrap()
 	}
 
+	callAndValidate(t, handler, "aah framework response writer test")
+}
+
+func TestNoStatusWritten(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		writer := WrapResponseWriter(w)
+
+		_, _ = writer.Write([]byte("aah framework no status written"))
+		assert.Equal(t, 31, writer.BytesWritten())
+	}
+
+	callAndValidate(t, handler, "aah framework no status written")
+}
+
+func TestMultipleStatusWritten(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		writer := WrapResponseWriter(w)
+
+		writer.WriteHeader(http.StatusOK)
+		writer.WriteHeader(http.StatusAccepted)
+
+		_, _ = writer.Write([]byte("aah framework mutiple status written"))
+		assert.Equal(t, 36, writer.BytesWritten())
+	}
+
+	callAndValidate(t, handler, "aah framework mutiple status written")
+}
+
+func TestHijackCall(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		writer := WrapResponseWriter(w)
+
+		con, rw, err := writer.(http.Hijacker).Hijack()
+		assert.FailOnError(t, err, "")
+		defer con.Close()
+
+		bytes := []byte("aah framework calling hijack")
+		rw.WriteString("HTTP/1.1 200 OK\r\n")
+		rw.WriteString("Date: " + time.Now().Format(http.TimeFormat) + "\r\n")
+		rw.WriteString("Content-Type: text/plain; charset=utf-8\r\n")
+		rw.WriteString("Content-Length: " + strconv.Itoa(len(bytes)))
+		rw.WriteString(string(bytes) + "\r\n")
+		_ = rw.Flush()
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+
+	http.Get(server.URL)
+}
+
+func TestCallCloseNotifyAndFlush(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		writer := WrapResponseWriter(w)
+
+		_, _ = writer.Write([]byte("aah framework calling close notify and flush"))
+		assert.Equal(t, 44, writer.BytesWritten())
+
+		writer.(http.Flusher).Flush()
+		ch := writer.(http.CloseNotifier).CloseNotify()
+		assert.NotNil(t, ch)
+	}
+
+	callAndValidate(t, handler, "aah framework calling close notify and flush")
+}
+
+func callAndValidate(t *testing.T, handler http.HandlerFunc, response string) {
 	server := httptest.NewServer(http.HandlerFunc(handler))
 	defer server.Close()
 
@@ -40,7 +109,8 @@ func TestResponseWriter(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Error Get: %v", err)
 	}
+	defer resp.Body.Close()
 
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	assert.Equal(t, "aah framework response writer test", string(bytes))
+	assert.Equal(t, response, string(bytes))
 }
