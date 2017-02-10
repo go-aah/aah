d03ceb48bc58b4ca5c7613c6979bb34f5e2d8e58
Jeevanandam M <jeeva@myjeeva.com>
2017-02-10 09:46:04 -0800

    support multiple closer

--- io.go
+++ io.go
@@ -8,8 +8,10 @@ import "io"
 
 // CloseQuietly closes `io.Closer` quietly. Very handy, where you do not care
 // about error while `Close()` and helpful for code quality too.
-func CloseQuietly(v interface{}) {
-	if d, ok := v.(io.Closer); ok {
-		_ = d.Close()
+func CloseQuietly(c ...interface{}) {
+	for _, v := range c {
+		if d, ok := v.(io.Closer); ok {
+			_ = d.Close()
+		}
 	}
 }
