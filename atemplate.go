73548935b84ed245b5dfc8892e7e7cfad9bb3062
Jeevanandam M <jeeva@myjeeva.com>
2017-03-05 21:22:30 -0800

    godoc update

--- atemplate.go
+++ atemplate.go
@@ -1,5 +1,5 @@
 // Copyright (c) Jeevanandam M. (https://github.com/jeevatkm)
-// go-aah/aah source code and usage is governed by a MIT style
+// go-aah/atemplate source code and usage is governed by a MIT style
 // license that can be found in the LICENSE file.
 
 package atemplate
