e4e371d0d73602c2e9c2ffeaa6ca112510aa2865
Jeevanandam M <jeeva@myjeeva.com>
2017-04-23 17:40:49 -0700

    graceful shutdown, aah framework requires go1.8 and above

--- server.go
+++ server.go
@@ -5,6 +5,7 @@
 package aah
 
 import (
+	"context"
 	"crypto/tls"
 	"errors"
 	"fmt"
@@ -96,6 +97,35 @@ func Start() {
 
 	// HTTP
 	startHTTP()
+}
+
+// Shutdown method allows aah server to shutdown gracefully with given timeoout
+// in seconds. It's invoked on OS signal `SIGINT` and `SIGTERM`.
+//
+// Method performs:
+//    - Graceful server shutdown with timeout by `server.timeout.grace_shutdown`
+//    - Publishes `OnShutdown` event
+//    - Exits program with code 0
+func Shutdown() {
+	graceTime := AppConfig().StringDefault("server.timeout.grace_shutdown", "60s")
+	if !(strings.HasSuffix(graceTime, "s") || strings.HasSuffix(graceTime, "m")) {
+		log.Warn("'server.timeout.grace_shutdown' value is not a valid time unit, assigning default")
+		graceTime = "60s"
+	}
+
+	graceTimeout, _ := time.ParseDuration(graceTime)
+	ctx, cancel := context.WithTimeout(context.Background(), graceTimeout)
+	if err := aahServer.Shutdown(ctx); err != nil {
+		log.Error(err)
+	}
+
+	// Publish `OnShutdown` event
+	AppEventStore().sortAndPublishSync(&Event{Name: EventOnShutdown})
+
+	// Exit normally
+	cancel()
+	log.Infof("'%v' application stopped", AppName())
+	os.Exit(0)
 }
 
 //‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
