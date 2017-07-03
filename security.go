6de33714f6e6c35ff89b0aa83067d1fbb08d97fd
Jeevanandam M <jeeva@myjeeva.com>
2017-07-03 15:57:19 -0700

    #37 auth module intergration progress

--- security.go
+++ security.go
@@ -6,31 +6,38 @@ package aah
 
 import (
 	"fmt"
-	"path/filepath"
 
+	ahttp "aahframework.org/ahttp.v0"
 	"aahframework.org/config.v0"
-	"aahframework.org/security.v0"
-	"aahframework.org/security.v0/session"
+	"aahframework.org/essentials.v0"
+	"aahframework.org/log.v0"
+	"aahframework.org/security.v0-unstable"
+	"aahframework.org/security.v0-unstable/authc"
+	"aahframework.org/security.v0-unstable/scheme"
+	"aahframework.org/security.v0-unstable/session"
 )
 
-const keySessionValues = "SessionValues"
+const (
+	keySessionValues = "_aahSessionValues"
+	keyAuthcInfo     = "_aahAuthcInfo"
+)
 
-var appSecurity *security.Security
+var appSecurityManager = security.New()
 
 //‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 // Global methods
 //___________________________________
 
-// AppSecurity method returns the application security instance,
+// AppSecurityManager method returns the application security instance,
 // which manages the Session, CORS, CSRF, Security Headers, etc.
-func AppSecurity() *security.Security {
-	return appSecurity
+func AppSecurityManager() *security.Manager {
+	return appSecurityManager
 }
 
 // AppSessionManager method returns the application session manager.
 // By default session is stateless.
 func AppSessionManager() *session.Manager {
-	return AppSecurity().SessionManager
+	return AppSecurityManager().SessionManager
 }
 
 // AddSessionStore method allows you to add custom session store which
@@ -40,15 +47,104 @@ func AddSessionStore(name string, store session.Storer) error {
 	return session.AddStore(name, store)
 }
 
+//‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+// Authentication and Authorization methods
+//__________________________________________
+
+func (e engine) handleAuthcAndAuthz(ctx *Context) flowResult {
+	// If route auth is `anonymous` then continue the request flow
+	// No authentication or authorization is required for that route.
+	if ctx.route.Auth == "anonymous" {
+		log.Debugf("Route auth is anonymous: %v", ctx.Req.Path)
+		return flowCont
+	}
+
+	authScheme := AppSecurityManager().GetAuthScheme(ctx.route.Auth)
+	if authScheme == nil {
+		// If auth scheme is nil then treat it as `anonymous`.
+		log.Infof("Route auth scheme is nil, treating as anonymous: %v", ctx.Req.Path)
+		return flowCont
+	}
+
+	log.Debugf("Route auth scheme: %s", authScheme.Scheme())
+	switch authScheme.Scheme() {
+	case "form":
+		return e.doFormAuthcAndAuthz(authScheme, ctx)
+	}
+
+	return flowCont
+}
+
+// doFormAuthcAndAuthz method does Form Authentication and Authorization.
+func (e *engine) doFormAuthcAndAuthz(ascheme scheme.Schemer, ctx *Context) flowResult {
+	formAuth := ascheme.(*scheme.FormAuth)
+
+	// In Form authentication check session is already authentication if yes
+	// then continue the request flow immediately.
+	if ctx.Subject().IsAuthenticated() {
+		if ctx.Session().IsKeyExists(keyAuthcInfo) {
+			ctx.Subject().AuthenticationInfo = ctx.Session().Get(keyAuthcInfo).(*authc.AuthenticationInfo)
+
+			// TODO cache for AuthorizationInfo
+			ctx.Subject().AuthorizationInfo = formAuth.DoAuthorizationInfo(ctx.Subject().AuthenticationInfo)
+		} else {
+			log.Warn("It seems there is an issue with session data of AuthenticationInfo")
+		}
+
+		return flowCont
+	}
+
+	// Check route is login submit URL otherwise send it login URL.
+	// Since session is not authenticated.
+	if formAuth.LoginSubmitURL != ctx.route.Path && ctx.Req.Method != ahttp.MethodPost {
+		loginURL := formAuth.LoginURL
+		if formAuth.LoginURL != ctx.Req.Path {
+			loginURL = fmt.Sprintf("%s?_rt=%s", loginURL, ctx.Req.Raw.RequestURI)
+		}
+		ctx.Reply().Redirect(loginURL)
+		e.writeReply(ctx)
+		return flowStop
+	}
+
+	// Do Authentication
+	// TODO publish pre auth server event
+	authcInfo, err := formAuth.DoAuthenticate(formAuth.ExtractAuthenticationToken(ctx.Req))
+	if err == authc.ErrAuthenticationFailed {
+		log.Infof("Authentication is failed, sending to login failure URL")
+		ctx.Reply().Redirect(formAuth.LoginFailureURL + "&_rt=" + ctx.Req.Raw.FormValue("_rt"))
+		e.writeReply(ctx)
+		return flowStop
+	}
+
+	ctx.Subject().AuthenticationInfo = authcInfo
+	ctx.Subject().AuthorizationInfo = formAuth.DoAuthorizationInfo(authcInfo)
+	ctx.Session().IsAuthenticated = true
+
+	// Remove the credential
+	ctx.Subject().AuthenticationInfo.Credential = nil
+	ctx.Session().Set(keyAuthcInfo, ctx.Subject().AuthenticationInfo)
+
+	// TODO publish post auth server event
+
+	rt := ctx.Req.Raw.FormValue("_rt")
+	if formAuth.IsAlwaysToDefaultTarget || ess.IsStrEmpty(rt) {
+		ctx.Reply().Redirect(formAuth.DefaultTargetURL)
+	} else {
+		log.Debugf("Redirect to URL found: %v", rt)
+		ctx.Reply().Redirect(rt)
+	}
+
+	e.writeReply(ctx)
+	return flowStop
+}
+
 //‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 // Unexported methods
 //___________________________________
 
-func initSecurity(cfgDir string, appCfg *config.Config) error {
-	var err error
-	configPath := filepath.Join(cfgDir, "security.conf")
-	if appSecurity, err = security.New(configPath, appCfg); err != nil {
-		return fmt.Errorf("security init: %s", err)
+func initSecurity(appCfg *config.Config) error {
+	if err := appSecurityManager.Init(appCfg); err != nil {
+		return err
 	}
 
 	// Based on aah server SSL configuration `http.Cookie.Secure` value is set, even
@@ -58,6 +154,14 @@ func initSecurity(cfgDir string, appCfg *config.Config) error {
 	}
 
 	return nil
+}
+
+func isFormAuthLoginRoute(ctx *Context) bool {
+	authScheme := AppSecurityManager().GetAuthScheme(ctx.route.Auth)
+	if authScheme != nil && authScheme.Scheme() == "form" {
+		return authScheme.(*scheme.FormAuth).LoginSubmitURL == ctx.route.Path
+	}
+	return false
 }
 
 //‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
--- security_test.go
+++ security_test.go
@@ -5,9 +5,18 @@
 package aah
 
 import (
+	"net/http/httptest"
+	"strings"
 	"testing"
 
-	"aahframework.org/security.v0/session"
+	"aahframework.org/ahttp.v0"
+	"aahframework.org/config.v0"
+	"aahframework.org/router.v0"
+	"aahframework.org/security.v0-unstable"
+	"aahframework.org/security.v0-unstable/authc"
+	"aahframework.org/security.v0-unstable/authz"
+	"aahframework.org/security.v0-unstable/scheme"
+	"aahframework.org/security.v0-unstable/session"
 	"aahframework.org/test.v0/assert"
 )
 
@@ -47,4 +56,106 @@ func TestSecuritySessionTemplateFuns(t *testing.T) {
 
 	v3 := tmplIsAuthenticated(viewArgs)
 	assert.False(t, v3)
+
+	delete(viewArgs, keySessionValues)
+	v4 := tmplIsAuthenticated(viewArgs)
+	assert.False(t, v4)
+}
+
+type testFormAuthentication struct {
+}
+
+func (tfa *testFormAuthentication) Init(cfg *config.Config) error {
+	return nil
+}
+
+func (tfa *testFormAuthentication) GetAuthenticationInfo(authcToken *authc.AuthenticationToken) *authc.AuthenticationInfo {
+	return testGetAuthenticationInfo()
+}
+
+func (tfa *testFormAuthentication) GetAuthorizationInfo(authcInfo *authc.AuthenticationInfo) *authz.AuthorizationInfo {
+	return nil
+}
+
+func TestSecurityHandleAuthcAndAuthz(t *testing.T) {
+	e := engine{}
+
+	// anonymous
+	r1 := httptest.NewRequest("GET", "http://localhost:8080/doc/v0.3/mydoc.html", nil)
+	ctx1 := &Context{
+		Req:   ahttp.ParseRequest(r1, &ahttp.Request{}),
+		route: &router.Route{Auth: "anonymous"},
+	}
+	result1 := e.handleAuthcAndAuthz(ctx1)
+	assert.True(t, result1 == flowCont)
+
+	// form auth scheme
+	cfg, _ := config.ParseString(`
+		security {
+		  auth_schemes {
+		    # HTTP Form Auth Scheme
+		    form_auth {
+		      scheme = "form"
+
+		      # Authenticator is used to validate the subject (aka User)
+		      authenticator = "security/Authentication"
+
+		      # Authorizer is used to get Subject authorization information,
+		      # such as Roles and Permissions
+		      authorizer = "security/Authorization"
+		    }
+		  }
+		}
+	`)
+	err := initSecurity(cfg)
+	assert.Nil(t, err)
+	r2 := httptest.NewRequest("GET", "http://localhost:8080/doc/v0.3/mydoc.html", nil)
+	w2 := httptest.NewRecorder()
+	ctx2 := &Context{
+		Req:     ahttp.ParseRequest(r2, &ahttp.Request{}),
+		Res:     ahttp.GetResponseWriter(w2),
+		route:   &router.Route{Auth: "form_auth"},
+		subject: &security.Subject{},
+		reply:   NewReply(),
+	}
+	result2 := e.handleAuthcAndAuthz(ctx2)
+	assert.True(t, result2 == flowStop)
+
+	// session is authenticated
+	ctx2.Session().IsAuthenticated = true
+	result3 := e.handleAuthcAndAuthz(ctx2)
+	assert.True(t, result3 == flowCont)
+
+	// form auth
+	testFormAuth := &testFormAuthentication{}
+	formAuth := AppSecurityManager().GetAuthScheme("form_auth").(*scheme.FormAuth)
+	err = formAuth.SetAuthenticator(testFormAuth)
+	assert.Nil(t, err)
+	err = formAuth.SetAuthorizer(testFormAuth)
+	assert.Nil(t, err)
+	r3 := httptest.NewRequest("POST", "http://localhost:8080/login", nil)
+	ctx2.Req = ahttp.ParseRequest(r3, &ahttp.Request{})
+	ctx2.Session().Set(keyAuthcInfo, testGetAuthenticationInfo())
+	result4 := e.handleAuthcAndAuthz(ctx2)
+	assert.True(t, result4 == flowCont)
+
+	// form auth not authenticated and no credentials
+	ctx2.Session().IsAuthenticated = false
+	delete(ctx2.Session().Values, keyAuthcInfo)
+	result5 := e.handleAuthcAndAuthz(ctx2)
+	assert.True(t, result5 == flowStop)
+
+	// form auth not authenticated and with credentials
+	r4 := httptest.NewRequest("POST", "http://localhost:8080/login", strings.NewReader("username=jeeva&password=welcome123"))
+	r4.Header.Set(ahttp.HeaderContentType, "application/x-www-form-urlencoded")
+	ctx2.Req = ahttp.ParseRequest(r4, &ahttp.Request{})
+	result6 := e.handleAuthcAndAuthz(ctx2)
+	assert.True(t, result6 == flowStop)
+}
+
+func testGetAuthenticationInfo() *authc.AuthenticationInfo {
+	authcInfo := authc.NewAuthenticationInfo()
+	authcInfo.Principals = append(authcInfo.Principals, &authc.Principal{Realm: "database", Value: "jeeva", IsPrimary: true})
+	authcInfo.Credential = []byte("$2y$10$2A4GsJ6SmLAMvDe8XmTam.MSkKojdobBVJfIU7GiyoM.lWt.XV3H6") // welcome123
+	return authcInfo
 }
