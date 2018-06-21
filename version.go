1ce0c2c4ee44ddaa5c1abe02b91bb0094bc6490f
Jeevanandam M <jeeva@myjeeva.com>
2018-06-20 21:18:40 -0700

    readme update for v0.8.0 release

--- version.go
+++ version.go
@@ -2,9 +2,9 @@
 // go-aah/essentials source code and usage is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-// Package ess provides simple & useful utils for Go. aah framework utilizes
-// essentials library across. It's pretty handy you can use it too :)
+// Package ess provides simple & useful util functions for Go. aah framework
+// utilizes essentials library across.
 package ess
 
 // Version no. of essentials library
-var Version = "0.8.0-edge"
+var Version = "0.8.0"
