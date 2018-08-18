6e08de1496385063c294f62efa1e29ca3f7bff69
Jeevanandam M <jeeva@myjeeva.com>
2018-08-18 16:40:57 -0700

    go-aah/aah#200 preparing view pkg move

--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jeevanandam M. (https://github.com/jeevatkm)
-// aahframework.org/view source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package view
-
-// Version no. of aah framework view library
-const Version = "0.9.0"
