c272028c8cbc97e9a50f9a0ff06bbf9184e6006e
Jeevanandam M <jeeva@myjeeva.com>
2018-08-18 14:20:31 -0700

    go-aah/aah#200 aruntime pkg into aah repo

--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jeevanandam M (https://github.com/jeevatkm)
-// go-aah/aruntime source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package aruntime
-
-// Version no. of aah framework aruntime library
-const Version = "0.3.1"
