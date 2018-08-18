bb8050a08f472a29e01728d1daa4e0acf90e6d7b
Jeevanandam M <jeeva@myjeeva.com>
2018-08-18 16:08:08 -0700

    go-aah/aah#200 preparing ainsp pkg move

--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jeevanandam M. (https://github.com/jeevatkm)
-// aahframework.org/ainsp source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package ainsp
-
-// Version no. of ainsp library for aah framework.
-const Version = "0.2.1"
