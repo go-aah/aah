078a78b00cee85e37811b783a4c34613a4d018ae
Jeevanandam M <jeeva@myjeeva.com>
2018-08-18 15:58:37 -0700

    go-aah/aah#200 preparing i18n pkg move

--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jeevanandam M. (https://github.com/jeevatkm)
-// aahframework.org/i18n source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package i18n
-
-// Version no. of aah framework i18n library
-const Version = "0.4.0"
