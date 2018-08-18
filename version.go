d0764b0897ee23f78bc8ef6345159bb63f90ba10
Jeevanandam M <jeeva@myjeeva.com>
2018-08-18 16:58:47 -0700

    go-aah/aah#200 preparing ahttp pkg move

--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jeevanandam M (https://github.com/jeevatkm)
-// aahframework.org/ahttp source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package ahttp
-
-// Version no. of aah framework ahttp library
-const Version = "0.11.3"
