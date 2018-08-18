6c28103f318ad09d2d88f922fc0f9f29d0111c22
Jeevanandam M <jeeva@myjeeva.com>
2018-08-18 16:23:00 -0700

    go-aah/aah#200 preparing config pkg move

--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jeevanandam M. (https://github.com/jeevatkm)
-// aahframework.org/config source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package config
-
-// Version no. of aahframework.org/config library
-var Version = "0.5.0"
