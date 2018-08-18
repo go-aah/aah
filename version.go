a6152b04516cd795e099ef61bc23b130a864af98
Jeevanandam M <jeeva@myjeeva.com>
2018-08-18 16:52:35 -0700

    go-aah/aah#200 preparing log pkg move

--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jeevanandam M (https://github.com/jeevatkm)
-// aahframework.org/log source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package log
-
-// Version no. of aahframework.org/log library
-const Version = "0.7.1"
