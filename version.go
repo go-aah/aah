63c82ea863b95ba9c531a02e775283111d0f2962
Jeevanandam M <jeeva@myjeeva.com>
2018-08-18 17:11:41 -0700

    go-aah/aah#200 preparing router pkg move

--- version.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jeevanandam M. (https://github.com/jeevatkm)
-// aahframework.org/router source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-package router
-
-// Version no. of aah framework router library
-const Version = "0.13.0"
