ae1f37d8db6e86e390e788d4bed3150c33279ace
Jeevanandam M <jeeva@myjeeva.com>
2018-10-25 09:33:16 -0700

    #217 added ability to include template from anywhere under view tree

--- view/funcs.go
+++ view/funcs.go
@@ -20,33 +20,38 @@ func (e *GoViewEngine) tmplSafeHTML(str string) template.HTML {
 // tmplInclude method renders given template with View Args and imports into
 // current template.
 func (e *GoViewEngine) tmplInclude(name string, viewArgs map[string]interface{}) template.HTML {
-	if !strings.HasPrefix(name, "common") {
+	if len(name) == 0 {
+		log.Error("goviewengine: empty template filename suppiled to func 'include'")
+		return e.tmplSafeHTML("")
+	}
+	if name[0] == '/' {
+		if strings.HasPrefix(name, "/views") {
+			name = strings.TrimPrefix(name, "/views")
+			log.Warnf("goviewengine: use without prefix of '/views' with func 'include' [/views%s => %s]", name, name)
+		}
+	} else if !strings.HasPrefix(name, "common") {
+		// TODO existing behaviour will be removed in the future release
 		name = "common/" + name
 	}
-
-	name = filepath.ToSlash(name)
 	var err error
 	var tmpl *template.Template
-	if e.hotReload {
+	if e.hotReload || name[0] == '/' {
 		if tmpl, err = e.ParseFile(name); err != nil {
 			log.Errorf("goviewengine: %s", err)
 			return e.tmplSafeHTML("")
 		}
 	} else {
-		tmpl = commonTemplates.Lookup(name)
+		tmpl = commonTemplates.Lookup(filepath.ToSlash(name))
 	}
-
 	if tmpl == nil {
 		log.Warnf("goviewengine: common template not found: %s", name)
 		return e.tmplSafeHTML("")
 	}
-
 	buf := acquireBuffer()
 	defer releaseBuffer(buf)
 	if err = tmpl.Execute(buf, viewArgs); err != nil {
 		log.Errorf("goviewengine: %s", err)
 		return e.tmplSafeHTML("")
 	}
-
 	return e.tmplSafeHTML(buf.String())
 }
